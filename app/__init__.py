3e08b10e6210614c05da39a57fc68237f2062e58
zaryabdar <zdar2005@gmail.com>
2026-07-29 00:23:32 +0500

    Initialize Databse and Create User Model

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from .config import Config
+from .Extensions import db,login_manager,migrate
 
-def Create_app():
+def create_app():
     app = Flask(__name__)
 
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    migrate.init_app(app,db)
+
     @app.route("/")
-    def main():
+    def home():
         return "Welcome to AI Blog"
 
+    from . import models
     return app
 
--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
+import os
+from dotenv import load_dotenv 
 
-class config:
-    SECRET_KEY ="Secret key"
-    SQLALCHEMY_DATABASE_URI = ""
+load_dotenv()
+
+class Config:
+    SECRET_KEY =os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
