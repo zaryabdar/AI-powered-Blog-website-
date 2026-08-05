a71e0d40f6dc3742c216749c3c3bf18ba04f34d4
zaryabdar <zdar2005@gmail.com>
2026-08-05 13:44:50 +0500

    Create Authentication and Post Routes

---
+++ app/routes/auth.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required, logout_user, login_user, current_user
+from forms.auth_forms import RegistrationForm,LoginForm
+from models.user import User
+from Extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth = Blueprint("auth",__name__)
+
+@auth.route("/register", methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_password = generate_password_hash(form.password.data)
+        user = User(
+            username = form.username.data,
+            email = form.email.data,
+            password_hash = hash_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully.", "success")
+        return redirect(url_for("auth.login"))
+    return render_template("register.html", form = form) 
+
+@auth.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email= form.email.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash("Welcome back!", "success")
+            if current_user.is_authenticated:
+                return redirect(url_for("main.dashboard"))
+        flash("Invalid email or password.", "danger")
+    return render_template("login.html", form = form)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out successfully.", "success")
+    return redirect(url_for("auth.login"))
--- app/routes/post.py
+++ app/routes/post.py
@@ -33,26 +33,36 @@ def create_post():
 
     return render_template("create_post.html",form = form)
 
-@post.route("/post/<int>id>/edit",methods=["GET","POST"])
+@post.route("/post/<int:id>/edit",methods=["GET","POST"])
 @login_required
 def update_post(id):
-    post = Post.query.filter_by(id =id).first()
+    post = Post.query.get_or_404(id)
     if post.author_id != current_user.id:
         abort(403)
     form =UpdatePostForm()
     if form.validate_on_submit():
-        post =Post(
-                title = form.title.data,
-                slug = "temporary slug",
-                summary = form.summary.data,
-                content = form.content.data,
-                cover_img = None,
-                author_id =current_user.id
-                )
-        db.session.add(post)
+        post.title = form.title.data
+        post.slug = "New temporary slug"
+        post.summary = form.summary.data
+        post.content = form.content.data
+        post.cover_img = None
         db.session.commit()
-        flash("Post Uploaded Successfully","success")
+        flash("Post Updated Successfully","success")
         return redirect(url_for("post.all_posts"))
+    if request.method == "GET":
+        form.title.data = post.title
+        form.summary.data = post.summary
+        form.content.data = post.content
+    return render_template("edit_post.html",form=form)
 
+@post.route("/post/<int:id>/delete",methods=["POST"])
+@login_required
+def delete_post(id):
+    post = Post.query.get_or_404(id)
+    if post.author != current_user:
+        abort(403)
 
-    
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post Deleted Successfully","success")
+    return redirect(url_for("post.all_posts"))
